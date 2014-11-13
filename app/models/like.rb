af4f08f43fb2273e4af8724d2fe3b0742a66f4f0
Klepsz <sklerenchyma@gmail.com>
2014-11-13 12:57:58 +0100

    Add Likes_count

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ActiveRecord::Base
   belongs_to :user
-  belongs_to :answer
+  belongs_to :answer, counter_cache: true
 end
---
+++ db/migrate/20141113113022_add_likes_count_to_answer.rb
@@ -0,0 +1,5 @@
+class AddLikesCountToAnswer < ActiveRecord::Migration
+  def change
+    add_column :answers, :likes_count, :integer, default: 0
+  end
+end
