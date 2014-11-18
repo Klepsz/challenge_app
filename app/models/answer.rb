db975fe24d7a431a2a1d628f12db659eac8419f9
Klepsz <sklerenchyma@gmail.com>
2014-11-18 16:24:31 +0100

    Add before_save :check_answer_acceptance to Answer.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,4 +4,13 @@ class Answer < ActiveRecord::Base
   has_many :likes
 
   validates :contents, presence: true
+
+  before_save :check_answer_acceptance
+
+  def check_answer_acceptance
+    if self.question.answers.where(accepted_answer: true).size == 1 && self.accepted_answer == true
+      errors.add(:answer, "You have accepted one answer already")
+      self.accepted_answer = false
+    end
+  end
 end
