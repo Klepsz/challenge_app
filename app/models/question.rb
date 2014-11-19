c9a80e0fcee74ddd40690255906ad27092ea4486
Klepsz <sklerenchyma@gmail.com>
2014-11-19 19:41:04 +0100

    Subtract points after creating a question

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,13 @@ class Question < ActiveRecord::Base
   has_many :answers
 
   validates :title, presence: true
+
+  after_save :subtract_points_after_creating_question
+
+  protected
+
+  def subtract_points_after_creating_question
+    user.points -= 10
+    user.save
+  end
 end
