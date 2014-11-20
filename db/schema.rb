cd2afc9cfa2c711293a32bfbd6c43266913c5e0c
Klepsz <sklerenchyma@gmail.com>
2014-11-20 08:25:26 +0100

    Add user_name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119154309) do
+ActiveRecord::Schema.define(version: 20141120072402) do
 
   create_table "answers", force: true do |t|
     t.integer  "question_id"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20141119154309) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "points",             default: 100
+    t.string   "user_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
