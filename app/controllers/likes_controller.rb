17262d2fc2a9246be1771071be0cd0f4c88ebbec
Klepsz <sklerenchyma@gmail.com>
2014-11-27 08:19:57 +0100

    Add Ajaxa

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,17 +2,21 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    answer = Answer.find(params[:answer_id])
+    # answer = Answer.find(params[:answer_id])
+    @answer = Answer.find(params[:answer_id])
 
-    if answer.user == current_user
-      redirect_to :back, alert: "You are not allowed to single your answer out!"
-    elsif
-      answer.likes.find_by(user: current_user) != nil
-      redirect_to :back, alert: "You have already liked this answer."
-    else      
-      @like = current_user.likes.build(answer: answer)
-      @like.save
-      redirect_to :back, notice: "You liked it"
+    respond_to do |format|
+      if @answer.user == current_user
+        format.html { redirect_to :back, alert: "You are not allowed to single your answer out!" }
+      elsif
+        @answer.likes.find_by(user: current_user) != nil
+        format.html { redirect_to :back, alert: "You have already liked this answer." }
+      else      
+        @like = current_user.likes.build(answer: @answer)
+        @like.save
+        format.html { redirect_to :back }
+      end
+      format.js
     end
   end
 
