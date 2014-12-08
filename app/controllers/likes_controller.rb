11e0c3f6a9a2ba336fd5c017c66048205acd82e4
Klepsz <sklerenchyma@gmail.com>
2014-12-08 19:06:30 +0100

    Adding like errors work with Ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,15 +2,16 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    # answer = Answer.find(params[:answer_id])
     @answer = Answer.find(params[:answer_id])
 
     respond_to do |format|
       if @answer.user == current_user
-        format.html { redirect_to :back, alert: "You are not allowed to single your answer out!" }
+        @error = "You are not allowed to single your answer out!"
+        format.html { redirect_to :back, alert: @error }
       elsif
         @answer.likes.find_by(user: current_user) != nil
-        format.html { redirect_to :back, alert: "You have already liked this answer." }
+        @error = "You have already liked this answer."
+        format.html { redirect_to :back, alert: @error }
       else      
         @like = current_user.likes.build(answer: @answer)
         @like.save
